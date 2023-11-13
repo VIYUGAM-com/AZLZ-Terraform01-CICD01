898a10c58937be4868efd7c106dbbe1d0913177f
chandiryn <apps.chandiran@gmail.com>
2023-11-13 22:16:54 +0530

    connectivity update

--- terraform-manifests/settings.connectivity.tf
+++ terraform-manifests/settings.connectivity.tf
@@ -56,7 +56,7 @@ locals {
           enabled = true
           config = {
             address_space                = ["10.132.0.0/16", ]
-            location                     = "westus"
+            location                     = "eastus"
             link_to_ddos_protection_plan = false
             dns_servers                  = []
             bgp_community                = ""
@@ -126,33 +126,33 @@ locals {
             azure_cache_for_redis                = true
             azure_cache_for_redis_enterprise     = true
             azure_container_registry             = true
-            azure_cosmos_db_cassandra            = true
-            azure_cosmos_db_gremlin              = true
-            azure_cosmos_db_mongodb              = true
-            azure_cosmos_db_sql                  = true
-            azure_cosmos_db_table                = true
+            azure_cosmos_db_cassandra            = false
+            azure_cosmos_db_gremlin              = false
+            azure_cosmos_db_mongodb              = false
+            azure_cosmos_db_sql                  = false
+            azure_cosmos_db_table                = false
             azure_data_explorer                  = true
-            azure_data_factory                   = true
-            azure_data_factory_portal            = true
-            azure_data_health_data_services      = true
-            azure_data_lake_file_system_gen2     = true
-            azure_database_for_mariadb_server    = true
+            azure_data_factory                   = false
+            azure_data_factory_portal            = false
+            azure_data_health_data_services      = false
+            azure_data_lake_file_system_gen2     = false
+            azure_database_for_mariadb_server    = false
             azure_database_for_mysql_server      = true
-            azure_database_for_postgresql_server = true
-            azure_digital_twins                  = true
-            azure_event_grid_domain              = true
-            azure_event_grid_topic               = true
-            azure_event_hubs_namespace           = true
+            azure_database_for_postgresql_server = false
+            azure_digital_twins                  = false
+            azure_event_grid_domain              = false
+            azure_event_grid_topic               = false
+            azure_event_hubs_namespace           = false
             azure_file_sync                      = true
             azure_hdinsights                     = true
-            azure_iot_dps                        = true
-            azure_iot_hub                        = true
+            azure_iot_dps                        = false
+            azure_iot_hub                        = false
             azure_key_vault                      = true
             azure_key_vault_managed_hsm          = true
             azure_kubernetes_service_management  = true
-            azure_machine_learning_workspace     = true
+            azure_machine_learning_workspace     = false
             azure_managed_disks                  = true
-            azure_media_services                 = true
+            azure_media_services                 = false
             azure_migrate                        = true
             azure_monitor                        = true
             azure_purview_account                = true
@@ -162,13 +162,13 @@ locals {
             azure_service_bus_namespace          = true
             azure_site_recovery                  = true
             azure_sql_database_sqlserver         = true
-            azure_synapse_analytics_dev          = true
-            azure_synapse_analytics_sql          = true
-            azure_synapse_studio                 = true
+            azure_synapse_analytics_dev          = false
+            azure_synapse_analytics_sql          = false
+            azure_synapse_studio                 = false
             azure_web_apps_sites                 = true
             azure_web_apps_static_sites          = true
             cognitive_services_account           = true
-            microsoft_power_bi                   = true
+            microsoft_power_bi                   = false
             signalr                              = true
             signalr_webpubsub                    = true
             storage_account_blob                 = true
@@ -177,12 +177,12 @@ locals {
             storage_account_table                = true
             storage_account_web                  = true
             enable_private_link_by_service       = true
-            azure_synapse_analytics_sqlserver    = true
+            azure_synapse_analytics_sqlserver    = false
 
           }
           private_link_locations = [
             "eastus",
-            "westus",
+            "eastus",
           ]
           public_dns_zones                                       = []
           private_dns_zones                                      = []
